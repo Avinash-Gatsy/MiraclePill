8cff9e167c6570097492004174405d075a4f1cae
Avinash <avinashg12291992@outlook.com>
2017-01-24 01:28:19 +0530

    UIKit ios ex1

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Avinash on 05/01/17.
+//  Copyright © 2017 avinash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Andhra","Karnataka","Kerala","Tamil Nadu","West Bengal","Maharastra"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        for i in 13...16 {
+            self.view.viewWithTag(i)?.isHidden = true
+        }
+        statePicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+        for i in 13...16 {
+            self.view.viewWithTag(i)?.isHidden = false
+        }
+    }
+}
+
